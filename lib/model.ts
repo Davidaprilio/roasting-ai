ac158eefb835c1a3db4bf189491c68941955e987
Davidaprilio <david1.aprilio4@gmail.com>
2024-10-05 01:35:39 +0700

    update model

--- lib/model.ts
+++ lib/model.ts
@@ -11,14 +11,12 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 export const modelFunRoaster = genAI.getGenerativeModel({
     // model: "gemini-1.5-pro-002",
-    model: "gemini-1.5-flash",
+    model: Math.random() < 0.3 ? "gemini-1.5-pro-002" : "gemini-1.5-flash",
     systemInstruction: [
-        "berperan sebagai profesional roaster dan comedian profesional yang kejam dan nyelekit tapi lucu.",
-        "buatlah roasting yang pedas dan lucu untuk sebuah profile yang diberikan",
-        "response roasting dengan komedi dalam kata gaul, kekinian, bahasa indonesia.",
+        "berperan sebagai profesional roaster dan comedian profesional yang kejam dan nyelekit",
+        "response roasting dengan komedi dalam kata gaul, kekinian, bahasa indonesia untu profil yang diberikan.",
         "berikan sedikit pujian atau tidak sama sekali dan itu pun sarkas.",
-        "jika kamu ingin memuji harus diikuti dengan roasting yang pedas dan kejam untuk menjatuhkan ekspektasi (puji sedikit lalu jatuhkan).",
-        "gunakan bahasa kekinian di indonesia seperti sipaling, sok iye, fomo, dan lainnya yang kamu tahu. (tidak harus menggunakan daftar kata ini hanya sebagai contoh dana jangan gunakan berlebihan)", 
+        "jika kamu ingin memuji harus diikuti dengan roasting yang pedas dan kejam (puji sedikit lalu jatuhkan).", 
     ].join('\n'),
     safetySettings: [
 
