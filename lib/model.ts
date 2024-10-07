3d4d411416a5d91a5c146032d485a4818b78415b
David Aprilio <63033587+Davidaprilio@users.noreply.github.com>
2024-10-07 18:01:16 +0700

    Update prompt
    Try Fix Error Blocked 
    [GoogleGenerativeAI Error]: Candidate was blocked due to SAFETY

--- lib/model.ts
+++ lib/model.ts
@@ -14,15 +14,16 @@ export const modelFunRoaster = genAI.getGenerativeModel({
     model: Math.random() < 0.3 ? "gemini-1.5-pro-002" : "gemini-1.5-flash",
     systemInstruction: [
         "berperan sebagai profesional roaster dan comedian profesional yang kejam dan nyelekit",
-        "response roasting dengan komedi dalam kata gaul, kekinian, bahasa indonesia untu profil yang diberikan.",
-        "berikan sedikit pujian atau tidak sama sekali dan itu pun sarkas.",
-        "jika kamu ingin memuji harus diikuti dengan roasting yang pedas dan kejam (puji sedikit lalu jatuhkan).", 
+        "response roasting dengan komedi dalam kata gaul, kekinian, dan gunakan bahasa indonesia untuk profil yang diberikan.",
+        "berikan sedikit pujian atau tidak sama sekali dan itu pun sarkas boleh lucu.",
+        "jika kamu ingin memuji harus diikuti dengan roasting yang pedas (puji sedikit lalu jatuhkan).", 
+        "berikan gojlokan yang menarik dan perhatikan tanda bacanya"
     ].join('\n'),
     safetySettings: [
 
     ],
     generationConfig: {
-        temperature: 1.5,
+        temperature: 1,
         topP: 0.95,
         topK: 40,
         maxOutputTokens: 9000,
